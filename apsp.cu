70ec6519a93fabc0d5a0fed0d572e7ceaa1c3f35
Bulat Nasrulin <bulat.nasrulin@gmail.com>
2016-09-30 17:00:38 +0300

    GPU L-opacity model

---
+++ apsp.cu
@@ -0,0 +1,93 @@
+#include "apsp.cuh"
+
+
+int main(int argc, char* argv[]){
+	char ch;
+	srand(time(NULL));
+	unsigned int t1_time = dtime_usec(0);
+	int vertex_number = std::atoi(argv[1]);
+	int l_value = std::atoi(argv[2]);
+	const int N =  vertex_number;
+	int L = l_value;
+	cout << "Vertex num " << N << " l value " << l_value << endl;
+	const int NumBytes = N*N*sizeof(int);
+	//host allocations to create Adjancency matrix and result matrices with path matrices
+	int *OrigGraph = (int *)malloc(NumBytes);//will be original Adjancency matrix, will NOT be changed
+	int *H_G = (int *)malloc(NumBytes);
+	int *H_Gpath = (int *)malloc(NumBytes);
+	int *D_G = (int *)malloc(NumBytes);
+	int *D_Gpath = (int *)malloc(NumBytes);
+
+  thrust::fill(H_Gpath, H_Gpath + N*N, -1);
+	thrust::fill(D_Gpath, D_Gpath + N*N, -1);
+	thrust::fill(H_G, H_G + N*N, INF);
+/*
+	for (int i = 0; i<N*N; i++){//copy for use in computation
+		//	H_G[i] = D_G[i] = OrigGraph[i];//copy for use in computation
+		H_Gpath[i] = D_Gpath[i] = -1;//set to all negative ones for use in path construction
+		H_G[i] = INF;
+	}
+*/
+	//Read file from file
+	cout << "Reading file with graph: \n ";
+//	_read_from_file(H_G, N);
+	_read_from_file(H_G, N);
+	cout << "Assigning initial paths ";
+
+
+	cout << "Graph " << endl;
+	for (int i = 0; i<N; i++){//copy for use in computation
+		{
+				H_G[i*N + i] = 0;
+			//			cout << H_G[i*N + j] << "  ";
+
+		}
+		//	cout << endl;
+	}
+
+	cout << "\nFloyd-Warshall on GPU underway:\n";
+	_Wake_GPU << <1, BLOCK_SIZE >> >(32);
+
+	//call host function which will copy all info to device and run CUDA kernels
+	t1_time = dtime_usec(t1_time);
+	unsigned int t2_time = dtime_usec(0);
+
+	
+	_GPU_Floyd(H_G, H_Gpath, N, L);
+	t2_time = dtime_usec(t2_time);
+
+
+//	printf("GPU Timing(including all device-host, host-device copies, device allocations and freeing of device memory): %dms\n\n", gpu_time);
+	std::cout<< "preprocessing time "  << t1_time/(float)USECPSEC << "Ffloyd Warshall gpu t2 time: " << t2_time/(float)USECPSEC << std::endl;
+	// The result is stored in H_G (the level)
+	/*
+	cout << "H_PATH" << endl;
+	for (int i = 0; i<N; i++){//copy for use in computation
+		for (int j = 0; j < N; j++)
+		{
+				cout << H_Gpath[i*N + j] << "  ";
+		}
+		cout << endl;
+	}
+
+	cout << "H_G" << endl;
+	for (int i = 0; i<N; i++){//copy for use in computation
+		for (int j = 0; j < N; j++)
+		{
+					cout << H_G[i*N + j] << "  ";
+		}
+			cout << endl;
+	}
+	*/
+
+	free(OrigGraph);
+	free(H_G);
+	free(H_Gpath);
+	free(D_G);
+	free(D_Gpath);
+	//}
+	//	*/
+
+	cin >> ch;
+	return 0;
+}
